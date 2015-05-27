336095b5c60f640e72a5a66c1b03e976727fa368
Richárd Laczkó <lacric85@users.noreply.github.com>
2015-05-27 18:56:37 +0200

    Update backup-home.sh

--- backup-home.sh
+++ backup-home.sh
@@ -18,10 +18,16 @@ DATE=`date +%Y-%m-%d--%H%M`
 FILENAME=`echo $NAME$SEP$DATE`
 
 ## Create the actual archive with verbose output saved to a log file:
-XZ_OPT=-9 tar -C $HOME -cvhJf $BACKUPDIR/$FILENAME.tar.xz $DIRLIST $FILELIST 2>&1 | \
+XZ_OPT=-9 XZ_DEFAULTS="-T 4" tar -C $HOME -cvhJf $BACKUPDIR/$FILENAME.tar.xz $DIRLIST $FILELIST 2>&1 | \
    tee $BACKUPDIR/$FILENAME.log
 
 ## Create another file containing only tar warnings/errors from log:
 cat $BACKUPDIR/$FILENAME.log | grep tar: > $BACKUPDIR/$FILENAME-tar.log
+
+dpkg --get-selections > $BACKUPDIR/$FILENAME.dpkg-list.txt
+apt-mark showauto > $BACKUPDIR/$FILENAME.dpkg-list-auto.txt
+# sudo sh -c "dpkg --clear-selections && dpkg --set-selections < $BACKUPDIR/$FILENAME.dpkg-list.txt && apt-get -f install && apt-mark auto 'cat $BACKUPDIR/$FILENAME.dpkg-list-auto.txt'""
+
+y-ppa-cmd advanced
 
 exit 0
